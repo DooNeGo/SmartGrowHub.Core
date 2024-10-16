bf3e5137483a7eedf3ca9588b8cda8bb9ac47328
Матвей Кострома <matveyprostomac@gmail.com>
2024-10-16 16:51:39 +0300

    Add errors

---
+++ SmartGrowHub.Domain/Errors/RefreshTokenExpiredError.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartGrowHub.Domain.Errors;
+
+public sealed record RefreshTokenExpiredError : Error
+{
+    public override string Message => "The refresh token has already expired";
+
+    public override bool IsExceptional => false;
+
+    public override bool IsExpected => true;
+
+    public override ErrorException ToErrorException() =>
+        ErrorException.New(Message);
+}
